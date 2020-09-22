ffce70e13d70387e70e4a8103578a955c0217eef
eukurapov <eukurapov@gmail.com>
2020-09-22 13:41:00 +0300

    Project 26 -- Challenge, supported finishing level + Switch to next level

--- 100DaysOfSwift/Project26/Project26/GameScene.swift
+++ 100DaysOfSwift/Project26/Project26/GameScene.swift
@@ -24,6 +24,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private var currentLevel = 1
     private var isGameOver = false
     
+    private var levelBlocks = [SKNode]()
+    
     private var scoreLabel: SKLabelNode!
     private var score = 0 {
         didSet {
@@ -73,18 +75,23 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
                 switch letter {
                 case "x":
                     let node = gameNodeFromImage("block", position: position, category: .block)
+                    levelBlocks.append(node)
                     addChild(node)
                 case "v":
                     let node = gameNodeFromImage("vortex", position: position, category: .vortex)
                     node.name = vortexName
                     node.run(SKAction.repeatForever(SKAction.rotate(byAngle: -.pi, duration: 1)))
+                    levelBlocks.append(node)
                     addChild(node)
                 case "s":
                     let node = gameNodeFromImage("star", position: position, category: .star)
                     node.name = starName
+                    levelBlocks.append(node)
                     addChild(node)
                 case "f":
                     let node = gameNodeFromImage("finish", position: position, category: .finish)
+                    node.name = finishName
+                    levelBlocks.append(node)
                     addChild(node)
                 case "p":
                     startPosition = position
@@ -160,8 +167,35 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
             let fadeOut = SKAction.scale(to: 0.0001, duration: 0.4)
             let remove = SKAction.removeFromParent()
             node.run(SKAction.sequence([scale, fadeOut, remove]))
+            if let index = levelBlocks.firstIndex(of: node) {
+                levelBlocks.remove(at: index)
+            }
         case finishName:
-            break
+            score += 5
+            player.physicsBody?.isDynamic = false
+            isGameOver = true
+            currentLevel += 1
+            let move = SKAction.move(to: node.position, duration: 0.25)
+            let fadeOut = SKAction.scale(to: 0.0001, duration: 0.25)
+            let remove = SKAction.removeFromParent()
+            player.run(SKAction.sequence([move, fadeOut, remove]))
+            let flip = SKAction.sequence([SKAction.scaleX(to: 0, duration: 0.2), SKAction.scaleX(to: -1, duration: 0.2)])
+            node.run(flip) { [unowned self] in
+                if currentLevel <= maxLevel {
+                    for block in levelBlocks {
+                        block.removeFromParent()
+                    }
+                    levelBlocks.removeAll(keepingCapacity: true)
+                    loadLevel()
+                    resetPlayer()
+                    isGameOver = false
+                } else {
+                    let gameOver = SKSpriteNode(imageNamed: "gameOver")
+                    gameOver.position = CGPoint(x: size.width/2, y: size.height/2)
+                    gameOver.zPosition = 2
+                    addChild(gameOver)
+                }
+            }
         default: return
         }
     }
@@ -174,4 +208,5 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     private let starName: String = "star"
     private let finishName: String = "finish"
     
+    private let maxLevel = 2
 }
