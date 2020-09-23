982728e770ca2452916ba29ce517aaafffe0de14
eukurapov <eukurapov@gmail.com>
2020-09-23 18:35:52 +0300

    Project 3 -- Chaalenge, added text on shared image

--- 100DaysOfSwift/Project3/Project1/DetailViewController.swift
+++ 100DaysOfSwift/Project3/Project1/DetailViewController.swift
@@ -38,26 +38,37 @@ class DetailViewController: UIViewController {
     }
     
     @objc func shareTapped() {
-        guard let image = imageView.image?.jpegData(compressionQuality: 0.8) else {
-            print("Image not found")
-            return
-        }
+        guard let image = watermarkedImage?.jpegData(compressionQuality: 0.8) else { return }
         
-        let vc = UIActivityViewController(activityItems: [image, imageTitle], applicationActivities: [])
+        let vc = UIActivityViewController(activityItems: [image, imageTitle!], applicationActivities: [])
         // to show it attached to bar button on iPad
         vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
         present(vc, animated: true)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    private var watermarkedImage: UIImage? {
+        guard let image = imageView.image else { return nil }
+        let rect = CGRect(origin: CGPoint(x: 0, y: 0), size: image.size)
+        let renderer = UIGraphicsImageRenderer(size: image.size)
+        let img = renderer.image { context in
+            image.draw(in: rect)
+            let paragraphStyle = NSMutableParagraphStyle()
+            paragraphStyle.alignment = .right
+            let text = "From Storm Viewer"
+            let attributes: [NSAttributedString.Key: Any] = [
+                .font: UIFont.systemFont(ofSize: 48),
+                .paragraphStyle: paragraphStyle,
+                .foregroundColor: UIColor.black,
+                .strokeColor: UIColor.white,
+                .strokeWidth: -3
+            ]
+            let attrString = NSAttributedString(string: text, attributes: attributes)
+            let textSize = attrString.size()
+            attrString.draw(
+                with: CGRect(origin: CGPoint(x: rect.maxX - textSize.width, y: rect.maxY - textSize.height), size: textSize),
+                options: .usesLineFragmentOrigin, context: nil)
+        }
+        return img
     }
-    */
 
 }
